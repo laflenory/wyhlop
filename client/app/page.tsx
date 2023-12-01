d8355a94a399597aa3d901c0ea372b9021337c42
Artyom Tarasov <laflenory@gmail.com>
2023-12-01 22:33:23 +0300

    [development] @wyhlop/client: Initial feedback section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,5 @@
-import { Wrapper } from '@/components';
+import { Feedback, Wrapper } from '@/components';
+import { FiMapPin, FiPhone, FiMail } from 'react-icons/fi';
 
 const Home = () => {
   return (
@@ -6,8 +7,39 @@ const Home = () => {
       <main>
         <section>
           <Wrapper>
-            <div>
-              <h1>Hello, World!</h1>
+            <div className='flex flex-col lg:flex-row'>
+              <div className='w-full p-8 lg:w-1/2'>
+                <h2 className='text-3xl text-zinc-900 font-bold'>Связаться</h2>
+                <p className='max-w-xl text-lg text-zinc-500 mt-4 mb-8'>
+                  Готовы ответить на ваши вопросы и предоставить необходимую
+                  информацию. Свяжитесь с нами, и мы постараемся оперативно
+                  помочь.
+                </p>
+                <ul className='flex flex-col gap-2 text-lg text-zinc-500 mb-8'>
+                  <li className='flex items-center gap-3'>
+                    <div className='text-zinc-900'>
+                      <FiMapPin />
+                    </div>
+                    г. Гродно, ул. Суворова, 109
+                  </li>
+                  <li className='flex items-center gap-3'>
+                    <div className='text-zinc-900'>
+                      <FiPhone />
+                    </div>
+                    +375 (29) 889-66-10
+                  </li>
+                  <li className='flex items-center gap-3'>
+                    <div className='text-zinc-900'>
+                      <FiMail />
+                    </div>
+                    laflenory@wyhlop.com
+                  </li>
+                </ul>
+                <div className='rounded-lg h-[280px] bg-zinc-100'></div>
+              </div>
+              <div className='w-full p-8 lg:w-1/2'>
+                <Feedback />
+              </div>
             </div>
           </Wrapper>
         </section>
---
+++ client/components/Feedback/Feedback.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Link from 'next/link';
+
+const Feedback = () => {
+  return (
+    <form className='flex flex-col gap-6'>
+      <div className='flex flex-row gap-6'>
+        <div className='w-full flex flex-col gap-4'>
+          <label className='text-zinc-900 font-semibold'>* Имя</label>
+          <input
+            placeholder='Введите имя'
+            className=' max-w-full text-zinc-900 outline-none ring-1 ring-inset ring-zinc-300 rounded-lg shadow-sm px-4 py-3 focus:ring-2 focus:ring-inset focus:ring-yellow-300'
+          />
+        </div>
+        <div className='w-full flex flex-col gap-4'>
+          <label className='text-zinc-900 font-semibold'>* Фамилия</label>
+          <input
+            placeholder='Введите фамилию'
+            className='max-w-full text-zinc-900 outline-none ring-1 ring-inset ring-zinc-300 rounded-lg shadow-sm px-4 py-3 focus:ring-2 focus:ring-inset focus:ring-yellow-300'
+          />
+        </div>
+      </div>
+      <div className='w-full flex flex-col gap-4'>
+        <label className='text-zinc-900 font-semibold'>* E-Mail</label>
+        <input
+          placeholder='Введите E-Mail'
+          className='text-zinc-900 outline-none ring-1 ring-inset ring-zinc-300 rounded-lg shadow-sm px-4 py-3 focus:ring-2 focus:ring-inset focus:ring-yellow-300'
+        />
+      </div>
+      <div className='w-full flex flex-col gap-4'>
+        <label className='text-zinc-900 font-semibold'>Номер телефона</label>
+        <input
+          placeholder='Введите номер телефона'
+          className='text-zinc-900 outline-none ring-1 ring-inset ring-zinc-300 rounded-lg shadow-sm px-4 py-3 focus:ring-2 focus:ring-inset focus:ring-yellow-300'
+        />
+      </div>
+      <div className='w-full flex flex-col gap-4'>
+        <label className='text-zinc-900 font-semibold'>* Сообщение</label>
+        <textarea
+          placeholder='Введите сообщение'
+          className='text-zinc-900 outline-none ring-1 ring-inset ring-zinc-300 rounded-lg shadow-sm px-4 py-3 resize-none h-[160px] focus:ring-2 focus:ring-inset focus:ring-yellow-300'
+        />
+      </div>
+      <div className='flex flex-col gap-6 md:items-end'>
+        <p className='text-center text-zinc-500 md:text-right'>
+          Отправляя эту форму, я соглашаюсь с{' '}
+          <Link href='/' className='text-yellow-400 font-semibold'>
+            политикой конфиденциальности
+          </Link>
+          .
+        </p>
+        <input
+          value='Отправить сообщение'
+          type='submit'
+          className='text-zinc-900 font-semibold outline-none rounded-lg bg-yellow-300 px-6 py-3 cursor-pointer hover:bg-yellow-200'
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Feedback;
